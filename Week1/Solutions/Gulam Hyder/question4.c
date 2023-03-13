367fe3364ceb46cb51dbf001eaad2a01e7e0d371
Hyder29 <gulamhaidersrk17@gmail.com>
2023-03-13 21:34:31 +0530

    Assignment Submitted

---
+++ Week1/Solutions/Gulam Hyder/question4.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+  float num1,num2;
+  char op;
+
+  printf("enter two numbers and the operator:");
+  scanf("%d %d %c",&num1,&num2,&op);
+ 
+   switch(op)
+   {
+    case '+':
+      printf("sum of two num is:%f",num1+num2);
+      break;
+    case '-':
+      printf("subtracton is:%f",num1+num2);
+      break;
+    case '*':
+      printf("multiplication is:%f",num1*num2);
+      break;
+    case '/':
+      printf("division is :%f",num1/num2);
+      break;
+    default:
+    printf("invalid");
+   }    
+      return 0;
+
+}
